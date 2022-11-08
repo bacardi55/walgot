9f97931b60396feb1d3a3f8bf28b32bdb8cb9918
bacardi55 <bac@rdi55.pl>
2022-11-08 21:09:41 +0100

    Split code for readability

--- internal/tui/utils.go
+++ internal/tui/utils.go
@@ -4,6 +4,10 @@ import (
 	"errors"
 	"os/exec"
 	"runtime"
+
+	"github.com/Strubbl/wallabago/v7"
+	"github.com/k3a/html2text"
+	"github.com/muesli/reflow/wordwrap"
 )
 
 // Open link in default browser.
@@ -26,4 +30,23 @@ func openLinkInBrowser(url string) error {
 		return err
 	}
 	return nil
+}
+
+// Retrieve index of the selected entry in model.Entries
+func getSelectedEntryIndex(entries []wallabago.Item, id int) int {
+	entryIndex := -1
+	for i := 0; i < len(entries); i++ {
+		if entries[i].ID == id {
+			return i
+		}
+	}
+
+	return entryIndex
+}
+
+// Retrieve the article content, in clean and wrap text.
+func getSelectedEntryContent(entries []wallabago.Item, index int) string {
+	contentHTML := entries[index].Content
+	content := html2text.HTML2Text(contentHTML)
+	return wordwrap.String(content, 72)
 }
